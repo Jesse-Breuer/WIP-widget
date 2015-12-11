ac8b7fcfec7e81a04aaa2b8cbfc904a7ef52b490
Steve Fulghum <steve@fulghum.digital>
2015-12-11 11:02:47 -0500

    add sass and angular material

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch',
+    // 'ngTouch',
+    'ngMaterial',
     'angular.filter'
   ])
   .config(function ($routeProvider) {
